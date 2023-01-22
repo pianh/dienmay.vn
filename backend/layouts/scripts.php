4c7b6b3a0d5d330dacf41f38e28b6f31b2237a3b
Duy Anh <aduy644@gmail.com>
2023-01-22 15:30:30 +0700

    add layouts file

---
+++ backend/layouts/scripts.php
@@ -0,0 +1,9 @@
+<!-- jQuery JS -->
+<script src="/dienmayct.vn/assets/vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap JS -->
+<!-- <script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script> -->
+
+<!-- Liên kết thư viện SweetAlert -->
+<script src="/dienmayct.vn/assets/vendor/sweetalert/sweetalert.js"></script>
+<script src="/dienmayct.vn/assets/vendor/sweetalert/sweetalert.min.js"></script>
---
+++ backend/layouts/style.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/dienmayct.vn/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
+<!-- Font awesome -->
+<link rel="stylesheet" href="/dienmayct.vn/assets/vendor/font-awesome/css/fontawesome.min.css" type="text/css" />
+
+<!-- Custom css -->
+<link rel="stylesheet" href="/dienmayct.vn/assets/backend/css/style.css" type="text/css" />
+<link rel="stylesheet" href="/dienmayct.vn/assets/backend/css/base.css" type="text/css" />
+<link rel="stylesheet" href="/dienmayct.vn/assets/backend/css/responsive.css" type="text/css" />
