27a4baf5b3e1c0a17700ca4656e2b6de94b92452
Duy Anh <aduy644@gmail.com>
2023-01-22 15:30:41 +0700

    add partials file

---
+++ backend/layouts/partials/header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+  <a class="navbar-brand" href="/">Nền tảng</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/index.php">Trang chủ Backtend</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav px-3 ml-auto">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="/dang-nhap.php">Đăng nhập</a>
+        </li>
+    </ul>
+  </div>
+</nav>
