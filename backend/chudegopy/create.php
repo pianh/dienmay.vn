45b7475df71b7429fa3c29e7e0616c3a89b2818d
Duy Anh <aduy644@gmail.com>
2023-01-29 01:29:59 +0700

    add code create file cdgy

---
+++ backend/chudegopy/create.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once __DIR__ . '/../layouts/meta.php'; ?>
+
+    <title>Thêm mới chủ đề góp ý</title>
+
+    <?php include_once __DIR__ . '/../layouts/styles.php'?>
+
+</head>
+<body>
+    <?php include_once __DIR__ . '/../layouts/partials/header.php' ?>
+    
+    <div class="container-fluid pb-500">
+        <div class="row">
+            <?php include_once __DIR__ . '/../layouts/partials/sidebar.php' ?>
+
+
+            <div class="col-md-10 pd-bottom-450">
+            </br>
+                <h1>Thêm mới chủ đề góp ý</h1>
+                <form name="frmCreate" id="frmCreate" method="post" action="">
+                    Tên chủ đề góp ý (*):
+                    <br/>
+                    <input type="text" name="cdgy_ten" id="cdgy_ten" class="form-control"/>
+                    <br />
+
+                    <button name="btnSave" id="btnSave" class="btn btn-primary">
+                    Lưu dữ liệu
+                    </button>
+                </form>
+                <?php
+                // Hiển thị tất cả lỗi trong PHP
+                // Chỉ nên hiển thị lỗi khi đang trong môi trường Phát triển (Development)
+                // Không nên hiển thị lỗi trên môi trường Triển khai (Production)
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
+                // Khi người dùng bấm lưu thì xử lý
+                if(isset($_POST['btnSave'])) {
+                    //1. Mở kết nối
+                    include_once __DIR__ . '/../../dbconnect.php';
+                    //2. chuẩn bị câu lệnh
+                    $ten = htmlentities ($_POST['cdgy_ten']);
+                    $sql = "INSERT INTO chudegopy(cdgy_ten) VALUES ('$ten');";
+                    //debug
+                    // var_dump($sql);
+                    // die;
+
+                    //3. Thực thi
+                    mysqli_query($conn, $sql);
+
+                    //4. Điều hướng
+                    //Điều hướng bằng JavaScrip
+                    echo '<script>location.href = "index.php"; </script>';
+
+                }
+
+                ?>
+
+
+
+
+            </div>
+        
+        </div>
+    </div>
+
+    
+
+    <?php include_once __DIR__ . '/../layouts/partials/footer.php' ?>
+    <?php include_once __DIR__ . '/../layouts/scripts.php' ?>
+</body>
+</html>
