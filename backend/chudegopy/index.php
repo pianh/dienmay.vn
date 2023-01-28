7e731ab4b480f6ab43637b94ccc0029974afa239
Duy Anh <aduy644@gmail.com>
2023-01-29 01:04:23 +0700

    add file styles.php of layouts

--- backend/chudegopy/index.php
+++ backend/chudegopy/index.php
@@ -1,6 +1,91 @@
-<?php
-    include_once __DIR__ . '/../../dbconnect.php';
-    $id=$_SESSION['tv_tendangnhap_logged'];
-    $sql = "SELECT * FROM khachhang WHERE kh_tendangnhap='$id';";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once __DIR__ . '/../layouts/meta.php'; ?>
+    <title>Danh sách các chủ đề góp ý</title>
+    <?php include_once __DIR__ . '/../layouts/styles.php'?>
+    
+</head>
+<body>
+    <?php include_once __DIR__ . '/../layouts/partials/header.php' ?>
+    
+    <div class="container-fluid pb-450">
+        <div class="row">
+            <?php include_once __DIR__ . '/../layouts/partials/sidebar.php' ?>
 
-?>
+
+            <div class="col-md-10">
+                </br>
+                <h1 class="text-center">Danh sách các chủ đề góp ý</h1>
+                <?php
+                    // Hiển thị tất cả lỗi trong PHP
+                    // Chỉ nên hiển thị lỗi khi đang trong môi trường Phát triển (Development)
+                    // Không nên hiển thị lỗi trên môi trường Triển khai (Production)
+                    ini_set('display_errors', 1);
+                    ini_set('display_startup_errors', 1);
+                    error_reporting(E_ALL);
+                    //1. Mở kết nối đến database
+                    include_once __DIR__ . '/../../dbconnect.php';
+                    //2. Chuan bi cau lenh
+                    $sql = "SELECT * FROM chudegopy
+                    order by cdgy_ma asc;";
+
+                    //3. Thuc thi
+                    $result = mysqli_query($conn, $sql);
+
+                    //4. Phân tách thành mảng array
+                    $data = [];
+                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        $data[] = array (
+                            'cdgy_ma' => $row['cdgy_ma'],
+                            'cdgy_ten' => $row['cdgy_ten']
+                            
+                        );
+                    }
+                    //  var_dump($data);
+                    //  die;
+                ?>
+                <a href="create.php" class="btn btn-success">Thêm mới</a>
+                </br></br>
+                <table class="table table-bordered table-hover">
+                        <thead class="table-info">
+                            <tr>
+                                <th>Mã chủ đề góp ý</th>
+                                <th>Tên chủ đề góp ý</th>
+                                <th>Hành động</th>
+                            </tr>
+                        </thead>
+                    <?php foreach($data as $cdgy): ?>
+                        <tr>
+                            <td><?= $cdgy['cdgy_ma']  ?>  </td>
+                            <td><?php echo $cdgy['cdgy_ten']  ?></td>
+                            <td>
+                                <!-- Nút sửa -->
+                                <a href="edit.php?cdgy_ma=<?= $cdgy['cdgy_ma']?>" class="btn btn-warning ">Sửa</a>
+                                <!-- Nút xóa -->
+                                <!-- <a href="delete.php?cdgy_ma=<?= $cdgy['cdgy_ma']?>" class="btn btn-danger">Xóa</a> -->
+                                <button type="button" class="btn btn-danger btnDelete" data-cdgy_ma="<?= $cdgy['cdgy_ma'] ?>">
+                                    Xóa
+                                </button>
+                                
+
+                                </form>
+                            </td>
+
+
+                        </tr>
+                    <?php endforeach; ?>
+                    </table>
+
+            </div>
+        
+        </div>
+    </div>
+
+    
+
+    <?php include_once __DIR__ . '/../layouts/partials/footer.php' ?>
+    <?php include_once __DIR__ . '/../layouts/scripts.php' ?>
+       
+</body>
+</html>
---
+++ backend/layouts/styles.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/dienmay.vn/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
+
+<!-- Font awesome -->
+<link rel="stylesheet" href="/dienmay.vn/assets/vendor/font-awesome/css/fontawesome.min.css" type="text/css" />
+
+<!-- Custom css -->
+<link rel="stylesheet" href="/dienmay.vn/assets/backend/css/styles.css" type="text/css" />
+<link rel="stylesheet" href="/dienmay.vn/assets/backend/css/base.css" type="text/css" />
+<link rel="stylesheet" href="/dienmay.vn/assets/backend/css/responsive.css" type="text/css" />
