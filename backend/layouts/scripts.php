d3c1ba7f8d52e80b5ff95a451b153ccd62894f44
Duy Anh <aduy644@gmail.com>
2023-01-29 01:31:49 +0700

    edit scripts file

--- backend/layouts/scripts.php
+++ backend/layouts/scripts.php
@@ -1,9 +1,9 @@
 <!-- jQuery JS -->
-<script src="/dienmayct.vn/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/dienmay.vn/assets/vendor/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap JS -->
-<!-- <script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script> -->
+<script src="/dienmay.vn/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 
 <!-- Liên kết thư viện SweetAlert -->
-<script src="/dienmayct.vn/assets/vendor/sweetalert/sweetalert.js"></script>
-<script src="/dienmayct.vn/assets/vendor/sweetalert/sweetalert.min.js"></script>
+<script src="/dienmay.vn/assets/vendor/sweetalert/sweetalert.js"></script>
+<script src="/dienmay.vn/assets/vendor/sweetalert/sweetalert.min.js"></script>
