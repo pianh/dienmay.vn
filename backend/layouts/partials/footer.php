04ebd801953b3153fea39425b4108519156f4320
Duy Anh <aduy644@gmail.com>
2023-02-06 17:56:45 +0700

    edit code partials

--- backend/layouts/partials/footer.php
+++ backend/layouts/partials/footer.php
@@ -3,8 +3,8 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 text-center"  >
-                <span class="text-white ">Bản quyền &copy; bởi <a style="color: #fff;" href="https://dienmayct">Duy Anh</a> - <script>document.write(new Date().getFullYear());</script>.</span>
-                <span style="color: #fff;">Học tại LearnForever.xyz để phát triển bản thân</span>
+                <span class="text-white ">Bản quyền &copy; bởi <a style="color: #fff;" href="https://dienmay.vn">Duy Anh</a> - <script>document.write(new Date().getFullYear());</script>.</span>
+                <span style="color: #fff;">Mua sắm tại Dienmay.vn</span>
                 <!-- class="text-muted" -->
             </div>
         </div>
--- backend/layouts/partials/sidebar.php
+++ backend/layouts/partials/sidebar.php
@@ -20,9 +20,44 @@
         </a>
         <ul class="collapse " id="qtvSubMenu" >
           <li class="nav-item sidebarSubMenu-list" >
-            <a href="/backend/chudegopy/index.php">Chủ đề GY</a>
+            <a href="/dienmay.vn/backend/khachhang/index.php">Khách hàng</a>
           </li>
-
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/chudegopy/index.php">Chủ đề GY</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/gopy/index.php">Góp ý</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/dondathang/index.php">Đơn đặt hàng</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/hinhsanpham/index.php">Hình sản phẩm</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/videosanpham/index.php">Video sản phẩm</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/hinhthucthanhtoan/index.php">Hình thức thanh toán</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/khuyenmai/index.php">Khuyến mãi</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/loaisanpham/index.php">Loại sản phẩm</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/nhasanxuat/index.php">Nhà sản xuất</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/sanpham/index.php">Sản phẩm</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/sanpham-dondathang/index.php">Sản phẩm - Đơn đặt hàng</a>
+          </li>
+          <li class="nav-item sidebarSubMenu-list" >
+            <a href="/dienmay.vn/backend/xuatbaocao/index.php">Xuất báo cáo</a>
+          </li>
         </ul>
       </li>
       <!-- End Menu KHU VỰC -->
