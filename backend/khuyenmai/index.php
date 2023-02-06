3e3debc7bb595efa793c67dff868f8d3d9fb79f5
Duy Anh <aduy644@gmail.com>
2023-02-06 17:56:04 +0700

    edit code index.php file khuyenmai

--- backend/khuyenmai/index.php
+++ backend/khuyenmai/index.php
@@ -1,67 +1,28 @@
-<?php
-// hàm `session_id()` sẽ trả về giá trị SESSION_ID (tên file session do Web Server tự động tạo)
-// - Nếu trả về Rỗng hoặc NULL => chưa có file Session tồn tại
-if (session_id() === '') {
-    // Yêu cầu Web Server tạo file Session để lưu trữ giá trị tương ứng với CLIENT (Web Browser đang gởi Request)
-    session_start();
-}
-
-if (!isset($_SESSION['tv_tendangnhap_logged'])){
-    echo '<script>location.href = "/learnforever.xyz/backend/auth/login.php";</script>';
-} 
-
-$id=$_SESSION['tv_tendangnhap_logged'];
-include_once __DIR__ . '/../../dbconnect.php';
-
-$sql = "SELECT * FROM thanhvien WHERE tv_tendangnhap='$id';";
-
-$result = mysqli_query($conn, $sql);
-
-//4. Phân tách thành mảng array
-$data = [];
-while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    $data[] = array (
-        'tv_ten' => $row['tv_ten'],
-        'tv_giaovien' => $row['tv_giaovien'],
-        'tv_quantri' => $row['tv_quantri']
-    );
-    $giaovien=$row['tv_giaovien'];
-    $quantri=$row['tv_quantri'];
-}
-
-if ($id!='admin' && $giaovien != '1' && $quantri != '1') {
-    $message = "Bạn không phải là thành viên quản trị website!";
-    echo "<script type='text/javascript'>alert('$message');</script>";
-    echo '<script>location.href = "/learnforever.xyz/index.php";</script>';
-    }
-else if ($id !='admin' && $quantri != 1) {
-    $message = "Chỉ quản trị viên mới có quyền này!";
-    echo "<script type='text/javascript'>alert('$message');</script>";
-    echo '<script>location.href = "/learnforever.xyz/backend/videokhoahoc/index.php";</script>';
-} 
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include_once __DIR__ . '/../layouts/meta.php'; ?>
 
-    <title>Danh sách các nhóm khóa học</title>
+    <title>Danh sách các chương trình khuyến mãi</title>
 
-    <?php include_once __DIR__ . '/../layouts/style.php'?>
+    <?php include_once __DIR__ . '/../layouts/styles.php'?>
+    <!-- DataTable CSS -->
+    <!-- <link href="/learnforever.xyz/assets/vendor/DataTables/datatables.min.css" type="text/css" rel="stylesheet" /> -->
+    <!-- <link href="/learnforever.xyz/assets/vendor/DataTables/Buttons-1.6.3/css/buttons.bootstrap4.min.css" type="text/css" rel="stylesheet" /> -->
     <style>
     </style>
 </head>
 <body>
     <?php include_once __DIR__ . '/../layouts/partials/header.php' ?>
     
-    <div class="container-fluid pd-bottom-100">
+    <div class="container-fluid pb-450" >
         <div class="row">
             <?php include_once __DIR__ . '/../layouts/partials/sidebar.php' ?>
 
 
             <div class="col-md-10">
             </br>
-            <h1>Danh sách các nhóm khóa học hiện có</h1>
+            <h1 class="text-center">Danh sách các chương trình khuyến mãi</h1>
             <?php
                     // Hiển thị tất cả lỗi trong PHP
                     // Chỉ nên hiển thị lỗi khi đang trong môi trường Phát triển (Development)
@@ -72,8 +33,8 @@ else if ($id !='admin' && $quantri != 1) {
                     //1. Mở kết nối đến database
                     include_once __DIR__ . '/../../dbconnect.php';
                     //2. Chuan bi cau lenh
-                    $sql = "SELECT * FROM nhomkhoahoc
-                    order by nkh_ma asc;";
+                    $sql = "SELECT * FROM khuyenmai
+                    order by km_ma asc;";
 
                     //3. Thuc thi
                     $result = mysqli_query($conn, $sql);
@@ -82,9 +43,11 @@ else if ($id !='admin' && $quantri != 1) {
                     $data = [];
                     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
                         $data[] = array (
-                            'nkh_ma' => $row['nkh_ma'],
-                            'nkh_ten' => $row['nkh_ten'],
-                            'nkh_mota' => $row['nkh_mota']
+                            'km_ma' => $row['km_ma'],
+                            'km_ten' => $row['km_ten'],
+                            'km_noidung' => $row['km_noidung'],
+                            'km_tungay' => $row['km_tungay'],
+                            'km_denngay' => $row['km_denngay']
                         );
                     }
 
@@ -95,26 +58,30 @@ else if ($id !='admin' && $quantri != 1) {
                 </br>
                 </br>
                     <table class="table table-bordered table-hover">
-                        <thead class="table-info">   
+                        <thead class="table-info">
                             <tr>
-                                <th>Mã Nhóm</th>
-                                <th>Tên Nhóm</th>
-                                <th>Mô tả nhóm</th>
+                                <th>Mã khuyến mãi</th>
+                                <th>Tên khuyến mãi</th>
+                                <th>Nội dung</th>
+                                <th>Từ ngày</th>
+                                <th>Đến ngày</th>
                                 <th>Hành động</th>
                             </tr>
                         </thead>
-                    <?php foreach($data as $nkh): ?>
+                    <?php foreach($data as $km): ?>
                         <tr>
-                            <td><?= $nkh['nkh_ma']  ?>  </td>
-                            <td><?php echo $nkh['nkh_ten']  ?></td>
-                            <td><?php echo $nkh['nkh_mota']  ?></td>
-
+                            <td><?= $km['km_ma']  ?>  </td>
+                            <td><?php echo $km['km_ten']  ?></td>
+                            <td><?php echo $km['km_noidung']  ?></td>
+                            <td><?php echo $km['km_tungay']  ?></td>
+                            <td><?php echo $km['km_denngay']  ?></td>
                             <td>
                                 <!-- Nút sửa -->
-                                <a href="edit.php?nkh_ma=<?= $nkh['nkh_ma']?>" class="btn btn-warning">Sửa</a>
+                                <a href="edit.php?km_ma=<?= $km['km_ma']?>" class="btn btn-warning">Sửa</a>
                                 <!-- Nút xóa -->
-                                <!-- <a href="delete.php?nkh_ma=<?= $nkh['nkh_ma']?>" class="btn btn-danger">Xóa</a> -->
-                                <button type="button" class="btn btn-danger btnDelete" data-nkh_ma="<?= $nkh['nkh_ma'] ?>">
+                                <!-- <a href="delete.php?km_ma=<?= $km['km_ma']?>" class="btn btn-danger">Xóa</a> -->
+                                <!-- Nút xóa, bấm vào sẽ xóa thông tin dựa vào khóa chính `km_ma` -->
+                                <button type="button" class="btn btn-danger btnDelete" data-km_ma="<?= $km['km_ma'] ?>">
                                     Xóa
                                 </button>
                                 <div style="padding-top: 10px;"></div>
@@ -135,7 +102,7 @@ else if ($id !='admin' && $quantri != 1) {
 
     <?php include_once __DIR__ . '/../layouts/partials/footer.php' ?>
     <?php include_once __DIR__ . '/../layouts/scripts.php' ?>
-    <!-- SweetAlert -->
+ <!-- SweetAlert -->
     <script>
    
             // Cảnh báo khi xóa
@@ -155,8 +122,8 @@ else if ($id !='admin' && $quantri != 1) {
 
                             // 3. Lấy giá trị của thuộc tính (custom attribute HTML) 'km_ma'
                             // var km_ma = $(this).attr('data-km_ma');
-                            var nkh_ma = $(this).data('nkh_ma');
-                            var url = "delete.php?nkh_ma=" + nkh_ma;
+                            var km_ma = $(this).data('km_ma');
+                            var url = "delete.php?km_ma=" + km_ma;
 
                             // Điều hướng qua trang xóa với REQUEST GET, có tham số km_ma=...
                             location.href = url;
@@ -169,5 +136,15 @@ else if ($id !='admin' && $quantri != 1) {
 
     </script>
 
+
+        
+
+
+
+
+
+    </script>
+
+    
 </body>
 </html>
