c21a9dad1c6215a3a9a04f223d2469f0f5976adf
Duy Anh <aduy644@gmail.com>
2023-02-06 17:57:19 +0700

    edit code dashboard.php file

--- backend/pages/dashboard.php
+++ backend/pages/dashboard.php
@@ -5,24 +5,22 @@
 
     <title>Bản tin Dashboard</title>
 
-    <?php include_once __DIR__ . '/../layouts/style.php'?>
-    <style>
-        /* .pd-500{
-            padding-bottom: 500px;
-        } */
-    </style>
+    <?php include_once __DIR__ . '/../layouts/styles.php'?>
 </head>
 <body >
     <?php include_once __DIR__ . '/../layouts/partials/header.php' ?>
     
-    <div class="container-fluid pd-350">
+    <div class="container-fluid pb-450">
         <div class="row">
             <?php include_once __DIR__ . '/../layouts/partials/sidebar.php' ?>
 
 
             <div class="col-md-10">
-                <h1>Bản tin Dashboard</h1>
+                <h1 class="text-center">Bản tin Dashboard</h1>
+                
 
+
+                
 
             </div>
         
