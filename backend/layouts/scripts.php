9fe79d04aa7512d7157e72e4df7d783a097d4fd1
Duy Anh <aduy644@gmail.com>
2023-02-08 00:44:02 +0700

    add link src DataTables scripts.php file

--- backend/layouts/scripts.php
+++ backend/layouts/scripts.php
@@ -10,3 +10,9 @@
 
 <!-- Chart JS-->
 <script src="/dienmay.vn/assets/vendor/chartjs/chart.min.js"></script>
+
+<!-- DataTable JS -->
+<script src="/dienmay.vn/assets/vendor/DataTables/datatables.min.js"></script>
+<script src="/dienmay.vn/assets/vendor/DataTables/Buttons-1.6.3/js/buttons.bootstrap4.min.js"></script>
+<script src="/dienmay.vn/assets/vendor/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+<script src="/dienmay.vn/assets/vendor/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
