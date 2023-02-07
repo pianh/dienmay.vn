b20c56b676351a5c830073ac972c3cc1a15206ab
Duy Anh <aduy644@gmail.com>
2023-02-07 15:27:36 +0700

    edit src styles.php file layouts

--- backend/layouts/styles.php
+++ backend/layouts/styles.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="/dienmay.vn/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
 
 <!-- Font awesome -->
-<link rel="stylesheet" href="/dienmay.vn/assets/vendor/font-awesome/css/fontawesome.min.css" type="text/css" />
+<link rel="stylesheet" href="/dienmay.vn/assets/vendor/font-awesome/css/all.min.css" type="text/css" />
 
 <!-- Custom css -->
 <link rel="stylesheet" href="/dienmay.vn/assets/backend/css/base.css" type="text/css" />
