62d1181bd5fcb06db5a10b3695fd7f1355ff722a
Duy Anh <aduy644@gmail.com>
2023-02-10 22:16:03 +0700

    edit code styles.php

--- backend/layouts/styles.php
+++ backend/layouts/styles.php
@@ -4,7 +4,11 @@
 <!-- Font awesome -->
 <link rel="stylesheet" href="/dienmay.vn/assets/vendor/font-awesome/css/all.min.css" type="text/css" />
 
+<!-- Paper CSS -->
+<!-- <link rel="stylesheet" href="/dienmay.vn/assets/vendor/paper-css/paper.css" type="text/css" /> -->
+
 <!-- Custom css -->
 <link rel="stylesheet" href="/dienmay.vn/assets/backend/css/base.css" type="text/css" />
 <link rel="stylesheet" href="/dienmay.vn/assets/backend/css/responsive.css" type="text/css" />
 <link rel="stylesheet" href="/dienmay.vn/assets/backend/css/styles.css" type="text/css" />
+
