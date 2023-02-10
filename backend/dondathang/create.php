b53e6ac9fc1a62396a662d19003a59de4cda85b4
Duy Anh <aduy644@gmail.com>
2023-02-10 22:16:53 +0700

    add code create.php file dondathang

--- backend/dondathang/create.php
+++ backend/dondathang/create.php
@@ -82,20 +82,29 @@
                                     <select name="kh_tendangnhap" id="kh_tendangnhap" class="form-control">
                                         <option value="">Vui lòng chọn khách hàng</option>
                                         <?php foreach ($datakhachhang as $khachhang):?>
-                                            <option value=" <?= $khachhang['kh_tendangnhap']  ?> "><?=$khachhang['kh_tomtat']?></option>
+                                            <option value="<?=$khachhang['kh_tendangnhap']?>"><?=$khachhang['kh_tomtat']?></option>
                                         <?php endforeach;?>
                                     </select>
                             </div>
                         </div>
                         <!-- End khách hàng -->
-                        <!-- Ngày lập đơn-->
+                        <!-- Ngày lập, ngày giao, nơi giao đơn-->
                         <div class="form-row">
-                            <div class="col-md-12 form-group">
-                            <label>Ngày lập</label>
-                                    <input type="datetime-local" name="dtt_ngaylap" id="dtt_ngaylap" class="form-control" />
+                            <div class="col-sm-4 form-group">
+                                <label>Ngày lập</label>
+                                <input type="datetime-local" name="dh_ngaylap" id="dh_ngaylap" class="form-control" />
                             </div>
+                            <div class="col-sm-4 form-group">
+                                <label>Ngày giao</label>
+                                <input type="datetime-local" name="dh_ngaygiao" id="dh_ngaygiao" class="form-control" />
+                            </div>
+                            <div class="col-sm-4 form-group">
+                                <label>Nơi giao</label>
+                                <input type="text" name="dh_noigiao" id="dh_noigiao" class="form-control" />
+                            </div>
+
                         </div>
-                        <!-- End Ngày lập đơn-->
+                        <!-- End Ngày lập, ngày giao, nơi giao đơn--->
                         <!-- Trạng thái và hình thức thanh toán -->
                         <div class="form-row">
                             <div class="col-md-6 form-group">
@@ -124,7 +133,7 @@
                         <!-- End Trạng thái và hình thức thanh toán -->
                     </fieldset>
 
-                    <fieldset name="chitietdonhangContainer" id="donhangContainer">
+                    <fieldset name="chiTietDonHangContainer" id="chiTietDonHangContainer">
                         <legend>Thông tin Chi tiết Đơn thanh toán</legend>
                         <div class="form-row">
                             <div class="form-group col-md-6">
@@ -138,16 +147,21 @@
                                 </select>
                                 <!-- end sản phẩm -->
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-3">
+                                <label>Số lượng</label><br />
+                                <input type="text" name="soluong" id="soluong" class="form-control" />
+                            </div>
+                            <div class="form-group col-md-3">
                                 <label>Xử lý</label><br />
                                 <button type="button" id="btnThemsanpham" class="btn btn-success">Thêm vào đơn đặt hàng</button>
                             </div>
                         </div>
                         <!-- Table chi tiết đơn hàng -->
                         <div class="form-group col-md-12" style="padding: 0;">
-                            <table id="tblchitietdonhang" class="table table-bordered">
+                            <table id="tblChiTietDonHang" class="table table-bordered">
                                 <thead>
                                     <th>Sản phẩm</th>
+                                    <th>Số lượng</th>
                                     <th>Đơn giá</th>
                                     <th>Thành tiền</th>
                                     <th>Hành động</th>
@@ -165,13 +179,48 @@
                 <?php
                 if (isset($_POST['btnSave'])) {
                     //Phân tách lấy dữ liệu người dùng gởi từ REQUEST POST
-                    $kh_tendangnhap = $_POST['tv_tendangnhap'];
+                    $kh_tendangnhap = $_POST['kh_tendangnhap'];
                     $dh_ngaylap = $_POST['dh_ngaylap'];
-                    $dh_trangthai = $_POST['dh_trangthai'];
+                    $dh_ngaygiao = $_POST['dh_ngaygiao'];
+                    $dh_noigiao = $_POST['dh_noigiao'];
+                    $dh_trangthaithanhtoan = $_POST['dh_trangthaithanhtoan'];
                     $httt_ma = $_POST['httt_ma'];
-
-
                     
+                    // Thông tin các dòng chi tiết đơn hàng
+                    $arr_sp_ma = $_POST['sp_ma'];                   // mảng array do đặt tên name="sp_ma[]"
+                    $arr_sp_dh_soluong = $_POST['sp_dh_soluong'];
+                    $arr_sp_dh_dongia = $_POST['sp_dh_dongia'];     // mảng array do đặt tên name="sp_dh_dongia[]"
+                    
+
+                    $sqlInsertdondathang = "INSERT INTO `dondathang` (`dh_ngaylap`, `dh_ngaygiao`, `dh_noigiao`, `dh_trangthaithanhtoan`, `httt_ma`, `kh_tendangnhap`) VALUES ('$dh_ngaylap', '$dh_ngaygiao', N'$dh_noigiao', '$dh_trangthaithanhtoan', '$httt_ma', '$kh_tendangnhap')";
+                    // print_r($sqlInsertdondathang); die;
+
+                    // Thực thi INSERT Đơn hàng
+                    mysqli_query($conn, $sqlInsertdondathang);
+
+                    // 3. Lấy ID Đơn hàng mới nhất vừa được thêm vào database
+                    // Do ID là tự động tăng (PRIMARY KEY và AUTO INCREMENT), nên chúng ta không biết được ID đă tăng đến số bao nhiêu?
+                    // Cần phải sử dụng biến `$conn->insert_id` để lấy về ID mới nhất
+                    // Nếu thực thi câu lệnh INSERT thành công thì cần lấy ID mới nhất của Đơn hàng để làm khóa ngoại trong Chi tiết đơn hàng
+                    $dh_ma = $conn->insert_id;
+
+                    // 4. Duyệt vòng lặp qua mảng các dòng Sản phẩm của chi tiết đơn hàng được gởi đến qua request POST
+                    for($i = 0; $i < count($arr_sp_ma); $i++) {
+                        // 4.1. Chuẩn bị dữ liệu cho câu lệnh INSERT vào table
+                        $sp_ma = $arr_sp_ma[$i];
+                        $sp_dh_soluong = $arr_sp_dh_soluong[$i];
+                        $sp_dh_dongia = $arr_sp_dh_dongia[$i];
+
+                        // 4.2. Câu lệnh INSERT
+                        $sqlInsertsanphamdondathang = "INSERT INTO `sanpham_dondathang` (`sp_ma`, `dh_ma`, `sp_dh_soluong`, `sp_dh_dongia`) VALUES ($sp_ma, $dh_ma, $sp_dh_soluong, $sp_dh_dongia)";
+                        
+                        // 4.3. Thực thi INSERT
+                        mysqli_query($conn, $sqlInsertsanphamdondathang);
+                    }
+
+                    // 5. Thực thi hoàn tất, điều hướng về trang Danh sách
+                    echo '<script>location.href = "index.php";</script>';
+
                 }
                 ?>
                 
@@ -187,7 +236,49 @@
     <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
     <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
 
-    <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
+    <script>
+        // Đăng ký sự kiện Click nút Thêm Sản phẩm
+               // Đăng ký sự kiện Click nút Thêm Sản phẩm
+        $('#btnThemsanpham').click(function() {
+            // debugger;
+            // Lấy thông tin Sản phẩm
+            var sp_ma = $('#sp_ma').val();
+            var sp_gia = $('#sp_ma option:selected').data('sp_gia');
+            var sp_ten = $('#sp_ma option:selected').text();
+            var soluong = $('#soluong').val();
+            var thanhtien = (soluong * sp_gia);
+            
+            // Tạo mẫu giao diện HTML Table Row
+            var htmlTemplate = '<tr>'; 
+            htmlTemplate += '<td>' + sp_ten + '<input type="hidden" name="sp_ma[]" value="' + sp_ma + '"/></td>';
+            htmlTemplate += '<td>' + soluong + '<input type="hidden" name="sp_dh_soluong[]" value="' + soluong + '"/></td>';
+            htmlTemplate += '<td>' + sp_gia + '<input type="hidden" name="sp_dh_dongia[]" value="' + sp_gia + '"/></td>';
+            htmlTemplate += '<td>' + thanhtien + '</td>';
+            htmlTemplate += '<td><button type="button" class="btn btn-danger btn-delete-row">Xóa</button></td>';
+            htmlTemplate += '</tr>';
+
+            // Thêm vào TABLE BODY
+            $('#tblChiTietDonHang tbody').append(htmlTemplate);
+
+            // Clear
+            $('#sp_ma').val('');
+            $('#soluong').val('');
+        });
+
+        // Đăng ký sự kiện cho tất cả các nút XÓA có sử dụng class .btn-delete-row
+        $('#chiTietDonHangContainer').on('click', '.btn-delete-row', function() {
+            // Ta có cấu trúc
+            // <tr>
+            //    <td>
+            //        <button class="btn-delete-row"></button>     <--- $(this) chính là đối tượng đang được người dùng click
+            //    </td>
+            // </tr>
+            
+            // Từ nút người dùng click -> tìm lên phần tử cha -> phần tử cha
+            // Xóa dòng TR
+            $(this).parent().parent()[0].remove();
+        });
+    </script>
 
 </body>
 
