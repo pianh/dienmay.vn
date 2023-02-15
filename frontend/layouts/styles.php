9fdd212cf364ed43ee760b75f789ea4bc7008ccf
Duy Anh <aduy644@gmail.com>
2023-02-16 00:30:52 +0700

    add structure file frontend

---
+++ frontend/layouts/styles.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/dienmay.vn/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
+
+<!-- Font awesome -->
+<link rel="stylesheet" href="/dienmay.vn/assets/vendor/font-awesome/css/all.min.css" type="text/css" />
+
+<!-- Paper CSS -->
+<!-- <link rel="stylesheet" href="/dienmay.vn/assets/vendor/paper-css/paper.css" type="text/css" /> -->
+
+<!-- Custom css -->
+<link rel="stylesheet" href="/dienmay.vn/assets/frontend/css/base.css" type="text/css" />
+<link rel="stylesheet" href="/dienmay.vn/assets/frontend/css/responsive.css" type="text/css" />
+<link rel="stylesheet" href="/dienmay.vn/assets/frontend/css/styles.css" type="text/css" />
+
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once __DIR__ . '/frontend/layouts/meta.php'; ?>
+    <title>Điện máy Cần Thơ Bán điện thoại đáng mua, phụ kiện, Tablet</title>
+    <?php include_once __DIR__ . '/frontend/layouts/styles.php'; ?>
+</head>
+<body>
+    <div class="container-fluid">
+        <?php include_once __DIR__ . '/frontend//layouts/partials/header.php' ?>
+        <main role="main">
+            <!-- Carousel  -->
+            <div class="slider">
+
+            </div>
+            <!-- End Carousel  -->
+
+            <!-- Tính năng marketing -->
+            <div class="marketing">
+
+            </div>
+            <!-- End Tính năng marketing -->
+
+            <!-- Info trang -->
+            <div class="">
+
+            </div>
+            <!-- End Info trang -->
+
+            <!-- Danh sách sản phẩm -->
+            <section class="">
+            </section>
+
+            <!-- End Danh sách sản phẩm -->
+        </main>
+    </div>
+    
+    <?php include_once __DIR__ . '/frontend/layouts/partials/footer.php' ?>
+    <?php include_once __DIR__ . '/frontend/layouts/scripts.php' ?>
+
+
+</body>
+</html>
